9c0eb309fd732e8434329ea4ddc9005f6221469c
andreydmitr21 <andreydmitr20@gmail.com>
2023-01-02 16:49:17 +0100

    First commit menu.html & our_story.html
    Changes to be committed:
    	modified:   Domaci 2/american food/css/main.css
    	modified:   Domaci 2/american food/css/menu.css
    	modified:   Domaci 2/american food/css/our_story.css
    	modified:   Domaci 2/american food/html/menu.html
    	modified:   Domaci 2/american food/html/our_story.html
    	modified:   Domaci 2/american food/index.html
    	modified:   Domaci 2/american food/js/index.js
    	modified:   Domaci 2/american food/js/main.js

--- Domaci 2/american food/js/main.js
+++ Domaci 2/american food/js/main.js
@@ -1,4 +1,4 @@
-//
+// resize event
 function windowResize () {
   menu('resize')
 }
@@ -12,4 +12,57 @@ window.onload = function () {
   menu('init')
 
   console.log('end of init')
+}
+
+// menu
+// menu for small screens
+var menu_shown = false
+function menu (job) {
+  const menu_sel = '.g1 .menu ul'
+
+  function menuHide () {
+    if (menu_shown) {
+      let ul = document.querySelector(menu_sel)
+      ul.style.display = 'none'
+      menu_shown = false
+    }
+  }
+
+  function menuShow () {
+    ul = document.querySelector(menu_sel)
+    ul.style.display = 'block'
+    menu_shown = true
+  }
+
+  function menuResize () {
+    if (window.innerWidth >= 600) {
+      menu_shown = false
+      ul = document.querySelector(menu_sel)
+      ul.style.display = null
+    }
+  }
+
+  console.log('menu.' + job)
+  switch (job) {
+    case 'init':
+      document.querySelector('.g1 .menu img').addEventListener('click', menu)
+
+      break
+    case 'hide':
+      menuHide()
+      break
+    case 'show':
+      menuShow()
+      break
+    case 'resize':
+      menuResize()
+      break
+    default:
+      if (menu_shown) {
+        menuHide()
+      } else {
+        menuShow()
+      }
+      break
+  }
 }
